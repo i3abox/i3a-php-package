b54ecf8e9d1bb3665b6d2b97dca4289e8a6f7853
overnick <overnic@httpd.cc>
2018-09-14 14:00:02 +0800

    增强模型分页数据

--- src/Traits/ModelPageOrAllTraits.php
+++ src/Traits/ModelPageOrAllTraits.php
@@ -27,7 +27,7 @@ trait ModelPageOrAllTrait
     public function scopeGetData($query, $column = ['*'], $num = 15)
     {
         if(app('request')->input('is_all')){
-            return $query->get($column);
+            return $this->scopeLimitData($query, $column);
         }else{
             return $this->scopeGetPage($query, $column, $num);
         }
@@ -45,6 +45,21 @@ trait ModelPageOrAllTrait
     {
         $pageNum = app('request')->input('page_num', $num);
         return $query->paginate($pageNum > 500 ? 500 : $pageNum, $column);
+    }
+
+    /**
+     * 筛选部分数据
+     *
+     * @param $query
+     * @param array $column
+     * @return mixed
+     */
+    public function scopeLimitData($query, $column = ['*'])
+    {
+        if(app('request')->has('page_num') && intval(app('request')->input('page_num')) > 0){
+            $query = $query->limit(app('request')->input('page_num'));
+        }
+        return $query->get($column);
     }
 
 }
